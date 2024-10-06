f09d2286ee2e39254a9f175974a13dc32b285643
KayyuruTharani <kayyurutharani@gmail.com>
2024-10-06 22:05:25 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">TABLEMATE</div>
+          <div className="right">
+            <p>NELLORE, ANDHRA PRADESH</p>
+            <p>Open: 10:00 AM - Closes: 10:00 PM(WEEKDAYS)</p>
+            <p>Open: 11:00 AM - Closes: 05:00 PM(WEEKENDS)</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
